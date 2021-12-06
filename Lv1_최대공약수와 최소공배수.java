068a5001bebb33d4d9102a2aeba919d0cc34b310
KyungMin0308 <70512325+KyungMin0308@users.noreply.github.com>
2021-12-06 18:06:24 +0900

    Update Lv1_최대공약수와 최소공배수.java

--- Lv1_최대공약수와 최소공배수.java
+++ Lv1_최대공약수와 최소공배수.java
@@ -1,16 +1,16 @@
 class Solution {
     
     //유클리드 호제법(최대 공약수)
-	//조건: a > b
-	private int GCD(int a, int b) {
-		while(b > 0) {
-			int tmp = a%b;
-			a = b;
-			b = tmp;
-		}
-		
-		return a;
+    //조건: a > b
+    private int GCD(int a, int b) {
+	while(b > 0) {
+	    int tmp = a%b;
+	    a = b;
+	    b = tmp;
 	}
+	
+	return a;
+    }
     
     public int[] solution(int n, int m) {
         int[] answer = new int[2]; //정답 배열
@@ -19,9 +19,9 @@ class Solution {
         
         //최대 공약수 구하기
         if(n > m)
-        	gcd = GCD(n, m);
+            gcd = GCD(n, m);
         else
-        	gcd = GCD(m, n);
+            gcd = GCD(m, n);
         
         //최소 공배수 구하기
         lcm = (n*m) / gcd;
